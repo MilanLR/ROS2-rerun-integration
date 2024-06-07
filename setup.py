3d68a11a29b09201d360a6517c65c862e8c1d9d5
pupper <mini@pupp.er>
2024-06-07 12:18:23 +0000

    Add `setup.py`

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+import os
+from glob import glob
+
+package_name = 'mini_pupper_rerun'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),       
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='ubuntu',
+    maintainer_email='ubuntu@todo.todo',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'rerun_subscriber = mini_pupper_rerun.rerun_subscriber:main'
+        ],
+    },
+)
