371acef13d56dac4578ba3b52cd79a9788a05858
Gideon <65892781+gideon-pol@users.noreply.github.com>
2024-06-26 15:08:27 +0200

    Added wall extraction and mesh construction

--- rerun_visualization/rerun_visualization/rerun_node.py
+++ rerun_visualization/rerun_visualization/rerun_node.py
@@ -285,22 +285,231 @@ class RerunNode(Node):
     def map_callback(self, msg, topic_name):
         image_data = np.ones((msg.info.height, msg.info.width, 3), dtype=np.uint8)
 
-        # transform = self.tf_buffer.lookup_transform("map", "odom", Time(), timeout=Duration(seconds=0.5))
+        transform = self.tf_buffer.lookup_transform("map", "base_footprint", Time(), timeout=Duration(seconds=10))
 
         for row in range(msg.info.height):
             for col in range(msg.info.width):
                 value = msg.data[row * msg.info.width + col]
                 if value == -1:
-                    image_data[row, col] = [255, 0, 0]  # Red for -1
+                    # image_data[row, col] = [255, 0, 0]  # Red for -1
+                    image_data[row, col] = [0, 0, 0]
                 else:
                     # Interpolate between black and white based on value
                     intensity = int((value / 100) * 255)
                     image_data[row, col] = [intensity, intensity, intensity]
 
-        # Log the occupancy grid as an image
+        # Convert the occupancy grid image to grayscale
+        gray_image = cv2.cvtColor(image_data, cv2.COLOR_BGR2GRAY)
+
+        # Apply Canny edge detection to the grayscale image
+        edges = cv2.Canny(gray_image, 100, 200)
+
+        # Apply Hough line transform to detect lines in the image
+        # lines = cv2.HoughLines(edges, 1, np.pi / 180, threshold=80)
+
+        # edged = image_data.copy()
+
+        # # Draw the detected lines on the original image
+        # if lines is not None:
+        #     for line in lines:
+        #         rho, theta, *_ = line[0]
+        #         a = np.cos(theta)
+        #         b = np.sin(theta)
+        #         x0 = a * rho
+        #         y0 = b * rho
+        #         x1 = int(x0 + 1000 * (-b))
+        #         y1 = int(y0 + 1000 * (a))
+        #         x2 = int(x0 - 1000 * (-b))
+        #         y2 = int(y0 - 1000 * (a))
+        #         cv2.line(edged, (x1, y1), (x2, y2), (0, 0, 255), 2)
+
+        # Apply Hough pobabilistic line transform to detect lines in the image
+        lines = cv2.HoughLinesP(edges, 1, np.pi / 180, threshold=60, minLineLength=10, maxLineGap=10)
+
+        def distance_to_line(point, line):
+            x1, y1, x2, y2 = line
+            x0, y0 = point
+            return np.abs((y2 - y1) * x0 - (x2 - x1) * y0 + x2 * y1 - y2 * x1) / np.sqrt((y2 - y1) ** 2 + (x2 - x1) ** 2)
+
+        def distance_to_point(point1, point2):
+            x1, y1 = point1
+            x2, y2 = point2
+            return np.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+
+        LINE_MERGE_THRESHOLD = 20
+
+        def lines_mergeable(line1, line2):
+            x1, y1, x2, y2 = line1[0]
+            x3, y3, x4, y4 = line2[0]
+
+            slope1 = (y2 - y1) / (x2 - x1) if x2 - x1 != 0 else 0
+            slope2 = (y4 - y3) / (x4 - x3) if x4 - x3 != 0 else 0
+            return  (
+                        abs(slope1 - slope2) < 0.5 or 
+                        distance_to_line((x1, y1), (x3, y3, x4, y4)) < LINE_MERGE_THRESHOLD and
+                        distance_to_line((x2, y2), (x3, y3, x4, y4)) < LINE_MERGE_THRESHOLD
+                    ) \
+                    and \
+                    (
+                        distance_to_point((x1, y1), (x3, y3)) < LINE_MERGE_THRESHOLD or
+                        distance_to_point((x1, y1), (x4, y4)) < LINE_MERGE_THRESHOLD or
+                        distance_to_point((x2, y2), (x3, y3)) < LINE_MERGE_THRESHOLD or
+                        distance_to_point((x2, y2), (x4, y4)) < LINE_MERGE_THRESHOLD
+                    )
+
+        def merge_lines(line1, line2):
+            x1, y1, x2, y2 = line1[0]
+            x3, y3, x4, y4 = line2[0]
+            all_points = [(x1, y1), (x2, y2), (x3, y3), (x4, y4)]
+            all_points.sort(key=lambda point: (point[0], point[1]))
+            return [[all_points[0][0], all_points[0][1], all_points[-1][0], all_points[-1][1]]]
+
+        filtered_lines = lines.tolist() if lines is not None else []
+            
+        # Modify the lines to go from left to right
+        for i, line in enumerate(filtered_lines):
+            x1, y1, x2, y2 = line[0]
+            if x1 > x2:
+                filtered_lines[i] = [[x2, y2, x1, y1]]
+
+        # Sort the lines by length
+        filtered_lines.sort(key=lambda line: distance_to_point((line[0][0], line[0][1]), (line[0][2], line[0][3])))
+
+        for i in range(0, len(filtered_lines)):
+            for j in range(i + 1, len(filtered_lines)):
+                if j >= len(filtered_lines):
+                    break
+                line1 = filtered_lines[i]
+                line2 = filtered_lines[j]
+                if line1 is not line2:
+                    x1, y1, x2, y2 = line1[0]
+                    x3, y3, x4, y4 = line2[0]
+                    if lines_mergeable(line1, line2):
+                        filtered_lines[i] = merge_lines(line1, line2)
+                        filtered_lines.remove(line2)
+                        j -= 1
+
+        edged = image_data.copy()
+
+        # Draw the detected lines on the original image
+        if lines is not None:
+            for line in lines:
+                x1, y1, x2, y2 = line[0]
+                cv2.line(edged, (x1, y1), (x2, y2), (0, 0, 255), 2)
+
+        edged_filtered = image_data.copy()
+
+        for line in filtered_lines:
+            x1, y1, x2, y2 = line[0]
+            cv2.line(edged_filtered, (x1, y1), (x2, y2), (255, 0, 0), 2)
+
+
+        map_origin = np.array([msg.info.origin.position.x, msg.info.origin.position.y, msg.info.origin.position.z])
+        # Calculate the position of the robot in the occupancy grid
+        robot_position = np.array([transform.transform.translation.x, transform.transform.translation.y, transform.transform.translation.z])
+        map_resolution = msg.info.resolution
+        map_origin = np.array([msg.info.origin.position.x, msg.info.origin.position.y, 0])
+        map_position = (robot_position - map_origin) / map_resolution
+
+        # Draw a circle on the occupancy grid image at the robot's position
+        cv2.circle(image_data, (int(map_position[0]), int(map_position[1])), 5, (0, 255, 0), -1)
+        cv2.circle(edged, (int(map_position[0]), int(map_position[1])), 5, (0, 255, 0), -1)
+        cv2.circle(edged_filtered, (int(map_position[0]), int(map_position[1])), 5, (0, 255, 0), -1)
+
         rr.log(
             f"{topic_name}/occupancy",
             rr.Image(image_data),
+        )
+
+        rr.log(
+            f"{topic_name}/occupancy_with_edges",
+            rr.Image(edges),
+        )
+
+        rr.log(
+            f"{topic_name}/occupancy_with_lines",
+            rr.Image(edged),
+        )
+
+        rr.log(
+            f"{topic_name}/occupancy_with_lines_filtered",
+            rr.Image(edged_filtered),
+        )
+
+        def construct_mesh(lines):
+            vertices = []
+            indices = []
+            for line in lines:
+                x1, y1, x2, y2 = line[0]
+
+                # Scale the line coordinates based on the map resolution
+                x1 *= map_resolution
+                y1 *= map_resolution
+                x2 *= map_resolution
+                y2 *= map_resolution
+
+                x1 += map_origin[0]
+                y1 += map_origin[1]
+                x2 += map_origin[0]
+                y2 += map_origin[1]
+
+                # Add vertices
+                vertices.append([x1, y1, 0])
+                vertices.append([x1, y1, 0.25])
+                vertices.append([x2, y2, 0])
+                vertices.append([x2, y2, 0.25])
+                # Add indices
+                indices.append(len(vertices) - 4)
+                indices.append(len(vertices) - 3)
+                indices.append(len(vertices) - 2)
+                indices.append(len(vertices) - 3)
+                indices.append(len(vertices) - 2)
+                indices.append(len(vertices) - 1)
+
+            return rr.Mesh3D(
+                vertex_positions=vertices,
+                triangle_indices=indices
+            )
+
+        mesh = construct_mesh(filtered_lines)
+        rr.log(
+            f"{topic_name}/occupancy_mesh",
+            mesh,
+        )
+
+        v_pos = [
+            [-1, -1, 0],
+            [1, -1, 0],
+            [-1, 1, 0],
+            [1, 1, 0]
+        ]
+
+        # Scale the vertex positions
+        scaled_v_pos = [
+            [
+                p[0] * 8 * msg.info.width / msg.info.height + msg.info.origin.position.x * map_resolution,
+                p[1] * 8 + msg.info.origin.position.y * map_resolution,
+                p[2] * 8 + msg.info.origin.position.z * map_resolution,
+            ]
+        for p in v_pos]
+
+        # Log the image_data on a quad
+        rr.log(
+            f"{topic_name}/occupancy_quad",
+            rr.Mesh3D(
+                vertex_positions=scaled_v_pos,
+                triangle_indices=[
+                    0, 1, 2,
+                    1, 2, 3
+                ],
+                vertex_texcoords=[
+                    [0, 0],
+                    [1, 0],
+                    [0, 1],
+                    [1, 1]
+                ],
+                albedo_texture=image_data,
+            )
         )
 
     def urdf_callback(self, urdf_msg, topic_name: str) -> None:
